c2bf7d7e40898118df33ada9d464a499e5e7c53e
Aaron Bentley <aaron@AB-5K-iMac.local>
2019-11-11 22:02:29 +0000

    Adding the foundations: - Theme UI - Firebase Setting up environment.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,48 @@
-import React from "react"
-import { Link } from "gatsby"
+/** @jsx jsx */
+import { Box } from '@theme-ui/components'
+import React from 'react'
+import { jsx } from 'theme-ui'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// <Layout>
+//     <SEO title="Home" />
+//     <h1>Hi people</h1>
+//     <p>Welcome to your new Gatsby site.</p>
+//     <p>Now go build something great.</p>
+//     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+//         <Image />
+//     </div>
+//     <Link to="/page-2/">Go to page 2</Link>
+// </Layout>
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <React.Fragment>
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                minHeight: '100vh',
+            }}>
+            <Box
+                sx={{
+                    p: 3,
+                }}>
+                Header
+            </Box>
+            <Box
+                sx={{
+                    flex: '1 1 auto',
+                    p: 3,
+                }}>
+                Content
+            </Box>
+            <Box
+                sx={{
+                    p: 3,
+                }}>
+                Footer
+            </Box>
+        </Box>
+    </React.Fragment>
 )
 
 export default IndexPage
