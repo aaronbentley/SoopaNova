77bf5143b7aaa52610f5bf5ed5115a0af4bb3afa
AaronBentley <aaronbentley@me.com>
2023-10-03 11:37:59 +0100

    Fix breaking changes in Radix UI/Shad CN.

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -40,7 +40,12 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-neutral-200 dark:bg-neutral-800" />
+    <ScrollAreaPrimitive.ScrollAreaThumb
+      className={cn(
+        "relative rounded-full bg-neutral-200 dark:bg-neutral-800",
+        orientation === "vertical" && "flex-1"
+      )}
+    />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
