3f3a6301b57380ce56b5c1787c4bd0925a634a4a
AaronBentley <aaronbentley@me.com>
2024-01-06 16:43:20 +0000

    Playing with order email notifications.

--- README.md
+++ README.md
@@ -4,16 +4,9 @@ Here's hoping I make something fun, cool and preferably, profitable.
 
 ## Content Todosies
 
-- [x] Frontpage - add frontpage section "Download your screenshots" 3 cards
-- [ ] Screenshots - how to get your screenshot files (Xbox/PlayStation/Steam?)
-- [x] About - what is SoopaNova?
-- [x] FAQ - add "how to download screenshots" - link to Screenshots page
-- [x] Not found - 404 page (image error icon)
-- [x] Privacy Policy
-- [x] Terms of Service
-- [ ] Thank you - Navigate to route on completed event?
+- [ ] Sustainability page?
 
 
 ## Features Todosies
 
-- [ ] SendGrid sales notification on Firestore document create
+- [ ] Admin email notification on new order, Firestore document create trigger
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,6 +8,7 @@
 import vision from '@google-cloud/vision'
 import { initializeApp } from 'firebase-admin/app'
 import { logger } from 'firebase-functions'
+import { onDocumentCreated } from 'firebase-functions/v2/firestore'
 import { onCall } from 'firebase-functions/v2/https'
 
 /**
@@ -127,6 +128,92 @@ export const moderateImageUrl = onCall(
             }
         } finally {
             logger.info('OK - Cloud Vision SafeSearch moderation completed.')
+        }
+    }
+)
+
+/**
+ * Firestore trigger function.
+ * Function to create a new order email confirmation and send it to admin.
+ */
+export const onOrderCreated = onDocumentCreated(
+    'customers/{userId}/orders/{orderId}',
+    async (event) => {
+        // Hardcode the notification destination email address for now
+        // const notificationInbox = 'aaronbentley@me.com'
+
+        // Hardcode the Resend API key for now
+        // const resend_api_key = 're_5LbNDjCq_CNkGN67RzuGPtZoG7DWa5yoD'
+        // Create a Resend instance
+        // const resend = new Resend(resend_api_key)
+
+        // Get an object representing the document
+        const snapshot = event.data
+
+        if (!snapshot) {
+            logger.error('Error: No document snapshot provided.')
+            return
+        }
+
+        // Get the document data
+        const data = snapshot.data()
+
+        if (!data) {
+            logger.error('Error: No document data provided.')
+            return
+        }
+
+        // Get the order data properties with  fallbacks
+        const order = {
+            id: snapshot.id,
+            'Created At': data.createdAt || 'not specified',
+            'Product Type': data.productType || 'not specified',
+            'Product Width': data.productWidth || 'not specified',
+            'Product Height': data.productHeight || 'not specified',
+            'Product Frame': data.productFrame || 'none',
+            'Product Edge': data.productEdge || 'none',
+            'Product Price': data.productPrice || 'not specified',
+            'Order Markup Rate': data.orderMarkupRate || 'not specified',
+            'Order Markup Profit': data.orderMarkupProfit || 'not specified'
+        }
+
+        // build html email
+        // const html = `
+        //     <h1>Order Created</h1>
+        //     <hr />
+        //     <p>Order ID: ${order.id}</p>
+        //     <p>Created At: ${order['Created At']}</p>
+        //     <p>Product Type: ${order['Product Type']}</p>
+        //     <p>Product Width: ${order['Product Width']}</p>
+        //     <p>Product Height: ${order['Product Height']}</p>
+        //     <p>Product Frame: ${order['Product Frame']}</p>
+        //     <p>Product Edge: ${order['Product Edge']}</p>
+        //     <p>Product Price ($): ${order['Product Price']}</p>
+        //     <p>Order Markup Rate (%): ${order['Order Markup Rate']}</p>
+        //     <p>Order Markup Profit ($): ${order['Order Markup Profit']}</p>
+        //     <hr />
+        // `
+
+        try {
+            // Send email
+            // const { data, error } = await resend.emails.send({
+            //     from: 'Soopanova <notifications@soopanova.app>',
+            //     to: [notificationInbox],
+            //     subject: 'Order Created',
+            //     html: html
+            // })
+
+            // if (error) {
+            //     throw new Error(error.message)
+            // }
+
+            // logger.info('OK - Order Created email sent.', data)
+
+            logger.info('OK', order)
+        } catch (error) {
+            let message = 'Something went wrong.'
+            if (error instanceof Error) message = error.message
+            console.error(message, error)
         }
     }
 )
